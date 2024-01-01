c84bbdef5c5752fe8baeff0c915200b9c51b7959
hesam hosseini <134384375+nuclearHesam@users.noreply.github.com>
2024-01-01 17:00:36 +0330

    adding files directory

--- Spotify-WpfClone/MainWindow.xaml.cs
+++ Spotify-WpfClone/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace Spotify_WpfClone
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
     }
 }
